25f95e78c15590a734616dd1c57e64873b27420a
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-06-10 20:11:37 +0300

    Fix header.

--- Bionessori.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Bionessori.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
