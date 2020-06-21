2aef8394ab3bffbd252cfac955cea78453213495
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-06-21 22:52:25 +0300

    [#62q8ft] Displaying user list in the table on the admin page.

--- Bionessori/Controllers/BackOfficeController.cs
+++ Bionessori/Controllers/BackOfficeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bionessori.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,21 @@ namespace Bionessori.Controllers {
     /// </summary>
     [ApiController, Route("api/back-office")]
     public class BackOfficeController : ControllerBase {
+        IBackOffice _office;
 
+        public BackOfficeController(IBackOffice office) {
+            _office = office;
+        }
+
+        /// <summary>
+        /// Метод получает список пользователей.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost, Route("get-users")]
+        public async Task<IActionResult> GetUsers() {
+            var oUsers = await _office.GetUsers();
+
+            return Ok(oUsers);
+        }
     }
 }
--- Bionessori/wwwroot/js/BackOffice.js
+++ Bionessori/wwwroot/js/BackOffice.js
@@ -2,7 +2,26 @@
 
 var admin = new Vue({
 	el: '#admin',
+	created() {
+		this.onInit();
+	},
+	data: {
+		aUsers: []
+	},
 	methods: {
+		onInit() {
+			const sUrl = "https://localhost:44312/api/back-office/get-users";
 
+			// Отправляет данные на Back-end.
+			axios.post(sUrl, {})
+				.then((response) => {
+					console.log("Список пользователей", response.data);
+
+					this.aUsers = response.data;					
+				})
+				.catch((XMLHttpRequest) => {
+					console.log("Ошибка получения списка пользователей", XMLHttpRequest.response.data);
+				});
+		}
 	}
 });
